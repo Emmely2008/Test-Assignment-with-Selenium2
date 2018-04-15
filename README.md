e4c85b98f10db57836881ab26822d76b976ac7ad
Emmely <e.a.c.l@hotmail.com>
2018-04-15 10:05:27 +0200

    Updates.

--- README.md
+++ README.md
@@ -1,1 +1,112 @@
-# Test-Assignment-with-Selenium2
+
+## Automated System Testing with Selenium2
+*Emmely Lundberg cph-el69*
+
+Demonstrate how to use Selenium to test a modern interactive (JavaScript driven) web application, and some of the problems involved with automated GUI testing1. 
+This is a StudyPoint exercise and will end up as part of an exam-question as sketched below: 
+ 
+_______________ 
+#### 1) Discuss Pros and Cons with manual versus automated tests 
+
+Manual tests takes longer time to implement but the time is gained be repeating the test again and again. We reuse the test in regression test if the test passes we can be assured that the program works.
+
+
+
+##### Pros
+- Fast and effective when performing the test.
+- Everyone can see results.
+- Take away manual repetitive boring tasks.
+- Take way the human factor of making mistakes in testing.
+
+##### Cons
+- Takes time to implement.
+- Require competes (usually developer implement them) to implement.
+- In the case of a project it requires resources usually taken from developers.
+- Less flexible than manual testing and has limitation.
+- Tools cost and require knowledge.
+
+
+Manual test can be cost effective in the short time. Easier to capture usability bugs than manual tests.
+_______________ 
+#### 2) Explain about the Test Pyramid and whether this exercise supports the ideas in the Test Pyramid 
+
+
+*Mike Cohn came up with this concept in his book Succeeding with Agile. 
+It's a great visual metaphor telling you to think about different layers of testing. 
+It also tells you how much testing to do on each layer.*[source](https://martinfowler.com/articles/practical-test-pyramid.html)
+
+[![https://gyazo.com/3b03458bfad91ee08975cfa348780fe1](https://i.gyazo.com/3b03458bfad91ee08975cfa348780fe1.png)](https://gyazo.com/3b03458bfad91ee08975cfa348780fe1)
+
+This exercise supports the idea because:
+
+- Given that this is just a snap short of the tests to be implemented for this application. I would say it is according to the pyramid. 
+We make a few test to check that the fron-end is "healthy". This aligned with the test pyramid be cause we are doing tests in the higher level and the pyramid suggest us to do fewer tests there.
+Checking for core functionality and that the GUI is healthy rather than checking lots of inputs with border values is an indication that the tests are reasonable low. 
+
+_______________ 
+#### 3) Discuss some of the problems with automated GUI tests and what makes such tests "vulnerable"  
+
+I see two vulnerabilities:
+
+1) We are limited in our testing such as testing for usability and that the layout hasn't changed.
+
+*Once you want to test for usability and a "looks good" factor you leave the realms of automated testing. 
+This is the area where you should rely on exploratory testing, usability testing (this can even be as simple as hallway testing) 
+and showcases with your users to see if they like using your product and can use all features without getting frustrated or annoyed.*
+
+2) The GUI is what the stockholders/users see and they might be eager to change around a lot. 
+It is also the easiest part to change but that might lead to the est needing to be rewritten so that that keeping the automated GUI tests creates a lot of maintenance.
+
+_______________ 
+#### 4) Demonstrate details in how to create a Selenium Test using the code for the exercise 
+
+_______________ 
+#### 5) Explain shortly about the DOM, and how you have read/manipulated DOM-elements in your test  
+
+*The Document Object Model (DOM) is a programming interface for HTML and XML documents. 
+It represents the page so that programs can change the document structure, style, and content. 
+The DOM represents the document as nodes and objects. That way, programming languages can connect to the page.
+
+A Web page is a document. This document can be either displayed in the browser window or as the HTML source. 
+But it is the same document in both cases. The Document Object Model (DOM) represents that same document so it can be manipulated. 
+The DOM is an object-oriented representation of the web page, which can be modified with a scripting language such as JavaScript.* [MDN Web Docs](https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Introduction)
+
+In the Selenium tests I use the Chrome Driver to read and manipulate the DOM.
+
+_______________ 
+#### 6) Explain how (and why it was necessary) you have solved "waiting" problems in your test 
+
+*When we perform a set of action on a web page the response time of the application might not be the same every time. 
+The DOM has not been rendered before we start performing actions on the page.
+
+*So, we need to inculcate a mechanism in our automation script that deals with such type of delays, this is nothing but Synchronization between our tool and the application under test.
+
+Synchronization/ Waits can be achieved in many different ways :
+
+- Sleep method of Thread class
+- Page Load timeout
+- Script timeout
+- Implicit Wait
+- Explicit Wait
+*
+I use:
+
+###### Implicit Wait :
+Implicit wait is a mechanism, which is used to notify WebDriver instance to wait for specific time, if any element is readily not available on the webpage.
+
+```
+	driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+
+```
+The synchronization mechanism is not associated with any particular web element, so it will be applicable for all the elements that our script interacts with, it is not highly appreciated.
+
+##### Script timeout :
+We can set the amount of time to wait for an asynchronous script to finish execution before throwing any error.
+
+```
+	driver.manage().timeouts().setScriptTimeout(100,TimeUnit.SECONDS);
+
+```
+Once added in the script, the WebDriver instance waits for 100 seconds for every asynchronous script to get executed on the web page before throwing an exception.
+
+[seleniumatfingertips.wordpress.com](https://seleniumatfingertips.wordpress.com/tag/setscripttimeout/)
--- src/test/java/MyClassTest.java
+++ src/test/java/MyClassTest.java
@@ -1,12 +1,16 @@
 import org.junit.*;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
@@ -15,11 +19,18 @@ import static org.junit.Assert.assertTrue;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MyClassTest {
     private static  WebDriver driver;
+
     @BeforeClass
     public static void setup(){
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\eacl\\Desktop\\TEST\\chromedriver_win32\\chromedriver.exe");
+
+        
         driver = new ChromeDriver();
+        //com.jayway.restassured.RestAssured.given().get("hhtp://localhost:3000/reset"); - caused errors
+        driver.get("http://localhost:3000/reset");
+
+        driver.manage().timeouts().setScriptTimeout(100,TimeUnit.SECONDS);
     }
     @AfterClass
     public static void tearDown(){
@@ -30,6 +41,11 @@ public class MyClassTest {
     @Test
     public void test1(){
         /**  1. Verify that data is loaded, and the DOM is constructed (Five rows in the table) */
+
+
+        driver.manage().timeouts().setScriptTimeout(100,TimeUnit.SECONDS);
+
+
         driver.get("http://localhost:3000/");
         assertTrue(driver.getTitle().startsWith("Document"));
         driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
@@ -40,32 +56,146 @@ public class MyClassTest {
     @Test
     public void test2(){
          /** 2. Write 2002 in the filter text and verify that we only see two rows*/
+        driver.findElement(By.id("filter")).sendKeys("2002");
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
 
-
+        int tableSize = driver.findElements(By.xpath("//table[@class='table']/tbody/tr")).size();
+        assertEquals(2, tableSize);
     }
     @Test
     public void test3() {
         /** 3. Clear the text in the filter text and verify that we have the original five rows  */
+        WebElement element = driver.findElement(By.id("filter"));
+        element.sendKeys(Keys.CONTROL + "a");
+        element.sendKeys(Keys.DELETE);
+
+
+        driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+        WebDriverWait wait2 = new WebDriverWait(driver,3);
+        driver.findElements(By.id("tbodycars"));
+        driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+
+
+        int tableSize = driver.findElements(By.xpath("//table[@class='table']/tbody/tr")).size();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+        assertEquals(5, tableSize);
+
     }
     @Test
     public void test4() {
         /** 4. Click the sort “button” for Year, and verify that the top row contains
          * the car with id 938 and the last row the car with id = 940.*/
+         //todo find the correct element to click
+        //todo query the two ids at top
+
+        // Reset
+        driver.get("http://localhost:3000/");
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("h_year")).click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+
+       List<WebElement> tr = driver.findElements(By.xpath("//table[@class='table']/tbody/tr"));
+        assertTrue(tr.get(0).getText().startsWith("938"));
+        assertTrue(tr.get(tr.size()-1).getText().startsWith("940"));
+
+
     }
     @Test
     public void test5() {
-        /** 5. Press the edit button for the car with the id 938. Change the Description to "Cool car", and save changes.
+        /** 5. Press the edit button for the car with the id 938.
+         * Change the Description to "Cool car", and save changes.
          * Verify that the row for car with id 938 now contains "Cool car" in the Description column */
+        //todo find car with i
+        //todo press the button
+        //todo Reset
+        driver.get("http://localhost:3000/");
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("h_year")).click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+
+        WebElement edit_link = driver.findElements(By.xpath("//table[@class='table']/tbody/tr[contains(td[1],\"938\")]/td[8]")).get(0);
+        edit_link.findElements(By.tagName("a")).get(0).click();
+
+        driver.findElement(By.id("description")).clear();
+        driver.findElement(By.id("description")).sendKeys("Cool car");
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+        driver.findElement(By.id("save")).click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+
+        System.out.println(driver.findElements(By.xpath("//table[@class='table']/tbody/tr[contains(td[1],\"938\")]/td[6]")).get(0).getText());
+        WebElement e = driver.findElements(By.xpath("//table[@class='table']/tbody/tr[contains(td[1],\"938\")]/td[6]")).get(0);
+        assertTrue("Cool car".equals(e.getText()));
+
+        //set description to cool car
+        //verify that the cool car contains the description
     }
     @Test
     public void test6() {
         /** 6. Click the new “Car Button”, and click the “Save Car” button. Verify that we have an error message with the
          * text “All fields are required” and we still only have five rows in the all cars table. */
+        //todo click new car button
+        //todo click save btn
+        //todo verify text
+        driver.findElement(By.id("new")).click();
+        driver.findElement(By.id("save")).click();
+
+        WebElement e = driver.findElements(By.id("submiterr")).get(0);
+        assertTrue("All fields are required".equals(e.getText()));
+
+
     }
     @Test
     public void test7() {
-        /** 6. 7. Click the new Car Button, and add the following values for a new car a. Year:   2008 b. Registered:  2002-5-5 c. Make:   Kia d. Model:   Rio e. Description:  As new f. Price:   31000
+        /** 6. 7. Click the new Car Button, and add the following values for a new car a.
+         *
+         * Year:   2008
+         * b. Registered:  2002-5-5
+         * c. Make:   Kia
+         * d. Model:   Rio
+         * e. Description:  As new
+         * f. Price:   31000
+         *
          Click “Save car”, and verify that the new car was added to the table with all the other cars. */
+        //todo
+        driver.findElement(By.id("new")).click();
+        driver.findElement(By.id("year")).sendKeys("2008");
+        driver.findElement(By.id("registered")).sendKeys("2002-5-5");
+        driver.findElement(By.id("make")).sendKeys("Kia");
+        driver.findElement(By.id("model")).sendKeys("Rio");
+        driver.findElement(By.id("description")).sendKeys("As new");
+        driver.findElement(By.id("price")).sendKeys("31000");
+
+        driver.findElement(By.id("save")).click();
+
+
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+
+        int tableSize = driver.findElements(By.xpath("//table[@class='table']/tbody/tr")).size();
+        assertEquals(6, tableSize);
+
+
+        System.out.println(driver.findElements(By.xpath("//table[@class='table']/tbody/tr[contains(td[2],\"2008\")]/td")).get(1).getText());
+        System.out.println(driver.findElements(By.xpath("//table[@class='table']/tbody/tr[contains(td[2],\"2008\")]/td")).get(2).getText());
+        System.out.println(driver.findElements(By.xpath("//table[@class='table']/tbody/tr[contains(td[2],\"2008\")]/td")).get(3).getText());
+        System.out.println(driver.findElements(By.xpath("//table[@class='table']/tbody/tr[contains(td[2],\"2008\")]/td")).get(4).getText());
+        System.out.println(driver.findElements(By.xpath("//table[@class='table']/tbody/tr[contains(td[2],\"2008\")]/td")).get(5).getText());
+        System.out.println(driver.findElements(By.xpath("//table[@class='table']/tbody/tr[contains(td[2],\"2008\")]/td")).get(6).getText());
+        List<WebElement> elms = driver.findElements(By.xpath("//table[@class='table']/tbody/tr[contains(td[2],\"2008\")]/td"));
+        assertTrue("2008".equals(elms.get(1).getText()));
+        assertTrue("5/5/2002".equals(elms.get(2).getText()));
+        assertTrue("Kia".equals(elms.get(3).getText()));
+        assertTrue("Rio".equals(elms.get(4).getText()));
+        assertTrue("As new".equals(elms.get(5).getText()));
+        assertTrue("31.000,00 kr.".equals(elms.get(6).getText()));
+
+
     }
+
+
+
+
+
 
 }
